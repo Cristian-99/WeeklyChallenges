e8ecf3af8c9008f0ce9d17538a2799ff2510e0a3
johnd <johndward01@yahoo.com>
2020-01-27 13:09:16 -0600

    finished

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,56 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+                return true;
+            else
+                return false;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+             double result = minuend - subtrahend;
+                return result;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int result = number1 + number2;
+            return result;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+                return number1;
+            else
+                return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            if (factor1 == 0 || factor2 == 0)
+                return 0;
+            else
+            {
+                long result = factor1 * factor2;
+                return result;
+            }
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson != null && nameOfPerson != "")
+                return $"Hello, {nameOfPerson}!";
+            else if (nameOfPerson == "")
+                return "Hello!";
+            else
+                return "Hello!";
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
